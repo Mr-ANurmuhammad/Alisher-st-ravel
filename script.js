3637617d6e36d2cfe6c1361c033959b311dbff07
Mr-ANurmuhammad <dweb43679@gmail.com>
2022-09-21 18:02:23 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Welcome");
+var sum = prompt("Pulingizni kiriting");
+if (sum >= 5450000) {
+   alert("Oq yo'l Alisher");
+} else if(sum < 5450000) {
+   alert("Azgina sabr qilishingizga to'g'ri keladi Alisher");
+}
+else {
+   alert ("Iltimos raqam kiriting");
+}
